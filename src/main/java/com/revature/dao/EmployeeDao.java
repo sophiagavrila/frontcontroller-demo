2d20baaa0069098f1d7c2e761c7f77fc3d19e40e
sophiagavrila <msophiagavrila@gmail.com>
2021-09-15 11:57:38 -0400

    add dao layer

---
+++ src/main/java/com/revature/dao/EmployeeDao.java
@@ -0,0 +1,76 @@
+package com.revature.dao;
+
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.revature.models.Employee;
+import com.revature.util.HibernateUtil;
+
+/*
+ * This class represents all of the CRUD operations for the Employee Table
+ * 
+ *
+ * Session methods:
+ * 
+ * save() and persist() --------result in a SQL insert
+ * update() and merge() --------result in a SQL update
+ * saveOrUpdate() --------------result in a SQL insert OR update (depends)
+ * get() and load() ------------result in a SQL select
+ *  
+ */
+public class EmployeeDao {
+	
+	public int insert(Employee e) {
+		
+		Session ses = HibernateUtil.getSession();
+		
+		Transaction tx = ses.beginTransaction();
+		
+		int pk = (int) ses.save(e);
+		
+		tx.commit();
+		
+		return pk;
+		
+	}
+	
+	public boolean update(Employee e) {
+		
+		
+		return false;
+	}
+	
+	public boolean delete(Employee e) {
+		
+		return false;
+	}
+	
+	public List<Employee> findAll() {
+		
+		Session ses = HibernateUtil.getSession();
+		
+		List<Employee> emps = ses.createQuery("from Employee", Employee.class).list();
+		
+		return emps;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
